9cd86181882abd1636578fe613874f8cfa02764f
Binaghi <binaghi_andres@hotmail.com>
2014-07-18 08:27:40 -0300

    Actualizacion

--- Base de Datos/ingenieria_2.sql
+++ Base de Datos/ingenieria_2.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 03-06-2014 a las 13:28:58
+-- Tiempo de generación: 18-07-2014 a las 01:18:59
 -- Versión del servidor: 5.5.24-log
 -- Versión de PHP: 5.4.3
 
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `administrador` (
   `dni` int(11) NOT NULL,
   `nombre_usuario` varchar(30) NOT NULL,
-  `contraseña` varchar(30) NOT NULL,
+  `clave` varchar(30) NOT NULL,
   `telefono` int(10) NOT NULL,
   `fecha_nac` date NOT NULL,
   `apellido` varchar(30) NOT NULL,
@@ -40,6 +40,13 @@ CREATE TABLE IF NOT EXISTS `administrador` (
   `piso` int(11) NOT NULL,
   PRIMARY KEY (`dni`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `administrador`
+--
+
+INSERT INTO `administrador` (`dni`, `nombre_usuario`, `clave`, `telefono`, `fecha_nac`, `apellido`, `nombre`, `calle`, `numero_casa`, `depto`, `piso`) VALUES
+(23456789, 'andres', 'clave2', 4532796, '2014-06-10', 'perez', 'andres', '54', 345, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -63,7 +70,7 @@ INSERT INTO `autor` (`id_autor`, `nombre_autor`, `apellido_autor`) VALUES
 (2, 'Kristen', 'Feola'),
 (3, 'Mirta G.', 'Carabajal'),
 (4, 'Petro c.', 'De Gandulfo'),
-(5, 'Christine', 'Bailey'),
+(5, 'Christian', 'Bayle'),
 (6, 'Toni', 'Rodriguez'),
 (7, 'Cecilia', 'Fassardi');
 
@@ -83,14 +90,14 @@ CREATE TABLE IF NOT EXISTS `categoria` (
 --
 
 INSERT INTO `categoria` (`nombre_categoria`) VALUES
-('criolla'),
-('cupcakes'),
-('guia'),
-('jugos'),
-('recetas'),
-('rustica'),
-('viandas'),
-('zumos');
+('Criolla'),
+('Cupcakes'),
+('Guia'),
+('Jugos'),
+('Recetas'),
+('Rusticas'),
+('Viandas'),
+('Zumos');
 
 -- --------------------------------------------------------
 
@@ -102,17 +109,28 @@ CREATE TABLE IF NOT EXISTS `cliente` (
   `id_cliente` int(11) NOT NULL AUTO_INCREMENT,
   `dni` int(11) NOT NULL,
   `nombre_usuario` varchar(30) NOT NULL,
-  `contraseña` varchar(30) NOT NULL,
-  `telefono` int(10) NOT NULL,
+  `clave` varchar(30) NOT NULL,
+  `telefono` int(15) NOT NULL,
   `fecha_nac` date NOT NULL,
   `apellido` varchar(30) NOT NULL,
   `nombre` varchar(30) NOT NULL,
+  `provincia` varchar(20) NOT NULL,
+  `localidad` varchar(20) NOT NULL,
   `calle` varchar(30) NOT NULL,
   `numero_casa` int(11) NOT NULL,
   `depto` int(11) NOT NULL,
   `piso` int(11) NOT NULL,
   PRIMARY KEY (`id_cliente`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Volcado de datos para la tabla `cliente`
+--
+
+INSERT INTO `cliente` (`id_cliente`, `dni`, `nombre_usuario`, `clave`, `telefono`, `fecha_nac`, `apellido`, `nombre`, `provincia`, `localidad`, `calle`, `numero_casa`, `depto`, `piso`) VALUES
+(1, 17456980, 'perezpepe', 'pepe', 2147483647, '1984-12-24', 'Perez', 'Pepe', 'Buenos Aires', 'La Plata', '76', 234, 0, 0),
+(2, 23907283, 'ItalyForce', 'ITALYFORCE', 2147483647, '1989-12-12', 'pascual', 'andres', 'buenos aires', 'la plata', 'calle 55', 234, 2, 3),
+(3, 22222222, 'qqqqqqq', 'qqqqqqq', 2147483647, '1995-12-12', 'sobrado', 'ariel', 'assaas', 'sasasasa', 'sasasa', 222, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -124,18 +142,17 @@ CREATE TABLE IF NOT EXISTS `editorial` (
   `id_editorial` int(11) NOT NULL AUTO_INCREMENT,
   `Nombre_Edi` varchar(30) NOT NULL,
   PRIMARY KEY (`id_editorial`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Volcado de datos para la tabla `editorial`
 --
 
 INSERT INTO `editorial` (`id_editorial`, `Nombre_Edi`) VALUES
-(1, 'DeBolsillo'),
+(1, 'De Bolsillo'),
 (2, 'Emece'),
-(3, 'santillana'),
-(4, 'puerto de palos'),
-(5, 'Pepe Pedro');
+(3, 'Santillana'),
+(4, 'Puerto de Palos');
 
 -- --------------------------------------------------------
 
@@ -165,13 +182,28 @@ CREATE TABLE IF NOT EXISTS `libro` (
 --
 
 INSERT INTO `libro` (`ISBN`, `stock_minimo`, `stock_actual`, `precio`, `titulo`, `idioma`, `paginas`, `fecha_publi`, `id_autor`, `id_editorial`, `oferta`) VALUES
-('123456789', 2, 7, 69, 'La gu&iacute;a optima para el ayuno de Daniel', 'Español', 68, '2001-08-25', 2, 4, 0),
-('1478523698', 4, 15, 47.8, 'Cupcakes veganos', 'Español', 55, '2011-01-02', 6, 1, 0),
+('1478523698', 4, 15, 47.8, 'Cupcakes veganos', 'Español', 55, '2011-01-02', 6, 1, 1),
+('1903479542', 6, 13, 79.84, 'El libro de las viandas mas practicas', 'Espanol', 87, '2012-01-01', 7, 4, 0),
+('2255748', 2, 3, 500, ' de Daniel', 'Español', 68, '2014-07-11', 1, 1, 0),
+('274694293', 5, 10, 58.99, 'Todo sobre la cocina Criolla', 'EspaÃ±ol', 87, '1983-03-31', 1, 4, 0),
+('290148481', 10, 57, 87.45, 'Las mejores recetas en 10 minutos', 'Espanol', 70, '2012-07-24', 3, 2, 0),
+('330294293', 5, 10, 58.99, 'Cocina local', 'EspaÃ±ol', 87, '1983-03-31', 1, 4, 0),
+('3698523257', 4, 15, 47.8, 'Cupcakes', 'EspaÃ±ol', 55, '2011-01-02', 6, 1, 0),
+('431044777', 3, 23, 152.21, 'Cocina popular', 'Espanol', 154, '2006-06-06', 4, 1, 1),
+('563287655', 6, 12, 99.99, 'La dieta de los zumos', 'EspaÃ±ol', 54, '1999-03-05', 5, 3, 0),
+('587956789', 2, 7, 69, 'La guia optima de Pedro', 'Espanol', 68, '2001-08-25', 2, 4, 0),
+('743887655', 6, 12, 99.99, 'La dieta de los 1000 zumos', 'EspaÃ±ol', 54, '1999-03-05', 5, 3, 1),
+('763256789', 2, 7, 69, 'La guia para los primeros pasos', 'Espanol', 68, '2001-08-25', 2, 4, 0),
+('7724523257', 4, 15, 47.8, 'Cupcakes para todos', 'EspaÃ±ol', 55, '2011-01-02', 6, 1, 0),
 ('8521479632', 6, 13, 79.84, 'El libro de las viandas para peque&ntilde;os', 'Español', 87, '2012-01-01', 7, 4, 0),
 ('878987655', 6, 12, 99.99, 'La dieta de los zumos', 'Español', 54, '1999-03-05', 5, 3, 0),
 ('879548481', 10, 57, 87.45, 'Las mejores recetas de rico y abundante', 'Español', 70, '2012-07-24', 3, 2, 0),
 ('882894293', 5, 10, 58.99, 'Cocina criolla', 'Español', 87, '1983-03-31', 1, 4, 0),
-('888444777', 3, 23, 152.21, 'Cocina con calor de hogar - rustica', 'Español', 154, '2006-06-06', 4, 1, 0);
+('888444777', 3, 23, 152.21, 'Cocina con calor de hogar - rustica', 'Español', 154, '2006-06-06', 4, 1, 0),
+('897348481', 10, 57, 87.45, 'Las mejores recetas de rico y facil', 'Espanol', 70, '2012-07-24', 3, 2, 0),
+('921444777', 3, 23, 152.21, 'Cocina para los rusticos', 'Espanol', 154, '2006-06-06', 4, 1, 0),
+('9502479542', 6, 13, 79.84, 'El libro de las viandas', 'Espanol', 87, '2012-01-01', 7, 4, 0),
+('asda', 0, 0, 0, '', '', 0, '0000-00-00', 1, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -191,14 +223,28 @@ CREATE TABLE IF NOT EXISTS `libro_categoria` (
 --
 
 INSERT INTO `libro_categoria` (`ISBN`, `nombre_categoria`) VALUES
-('882894293', 'criolla'),
-('1478523698', 'cupcakes'),
-('123456789', 'guia'),
-('878987655', 'jugos'),
-('879548481', 'recetas'),
-('888444777', 'rustica'),
-('8521479632', 'viandas'),
-('878987655', 'zumos');
+('274694293', 'Criolla'),
+('882894293', 'Criolla'),
+('1478523698', 'Cupcakes'),
+('3698523257', 'Cupcakes'),
+('7724523257', 'Cupcakes'),
+('587956789', 'Guia'),
+('763256789', 'Guia'),
+('563287655', 'Jugos'),
+('743887655', 'Jugos'),
+('878987655', 'Jugos'),
+('290148481', 'Recetas'),
+('879548481', 'Recetas'),
+('897348481', 'Recetas'),
+('330294293', 'Rusticas'),
+('431044777', 'Rusticas'),
+('888444777', 'Rusticas'),
+('921444777', 'Rusticas'),
+('1903479542', 'Viandas'),
+('8521479632', 'Viandas'),
+('9502479542', 'Viandas'),
+('563287655', 'Zumos'),
+('878987655', 'Zumos');
 
 -- --------------------------------------------------------
 
@@ -227,12 +273,22 @@ CREATE TABLE IF NOT EXISTS `venta` (
   `id_venta` int(11) NOT NULL AUTO_INCREMENT,
   `fecha_venta` date NOT NULL,
   `estado` varchar(30) NOT NULL,
-  `hora` time NOT NULL,
   `cant_venta` int(11) NOT NULL,
+  `precio` int(11) NOT NULL,
   `id_cliente` int(11) NOT NULL,
   `ISBN` int(11) NOT NULL,
   PRIMARY KEY (`id_venta`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=27 ;
+
+--
+-- Volcado de datos para la tabla `venta`
+--
+
+INSERT INTO `venta` (`id_venta`, `fecha_venta`, `estado`, `cant_venta`, `precio`, `id_cliente`, `ISBN`) VALUES
+(2, '0000-00-00', 'Procesando', 3, 143, 0, 1478523698),
+(24, '2014-07-17', 'Entregado', 4, 191, 1, 1478523698),
+(25, '2014-07-17', 'Procesando', 4, 191, 1, 1478523698),
+(26, '2014-07-17', 'Procesando', 7, 483, 1, 123456789);
 
 --
 -- Restricciones para tablas volcadas
